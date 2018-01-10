f37e8350352ad96ca831ab7dac26f762087ad8ba
Loongy <benjamin.jl.wang@gmail.com>
2018-01-10 12:34:06 +1100

    Updated comments and param naming style to be consistent across RepublicToken and Utils

--- contracts/Utils.sol
+++ contracts/Utils.sol
@@ -5,18 +5,18 @@ library Utils {
   /**
    * @notice Create a new bytes array containing the last n bytes of the input.
    *
-   * @param bs The input bytes.
-   * @param n The number of bytes that will be taken from the end of the input
+   * @param _bs The input bytes.
+   * @param _n The number of bytes that will be taken from the end of the input
    *        bytes.
    *
    * @return The last n bytes of the input bytes.
    */
-  function lastNBytes(bytes bs, uint n) public pure returns (bytes out) {
-    assert(bs.length <= n);
-    out = new bytes(n);
-    uint offset = bs.length - n;
-    for (uint i = 0; i < n; i++) {
-      out[i] = bs[offset + i];
+  function lastNBytes(bytes _bs, uint _n) public pure returns (bytes out) {
+    assert(_bs.length <= _n);
+    out = new bytes(_n);
+    uint offset = _bs.length - _n;
+    for (uint i = 0; i < _n; i++) {
+      out[i] = _bs[offset + i];
     }
     return out;
   }
@@ -26,48 +26,48 @@ library Utils {
    * public key is 65 bytes (1 byte 0x04, 32 bytes x value, 32 bytes y value).
    * The address is taken from only the last 64 bytes.
    *
-   * @param publicKey The public key.
+   * @param _publicKey The public key.
    *
    * @return An Ethereum address.
    */
-  function ethereumAddressFromPublicKey(bytes publicKey) public pure returns (address) {
+  function ethereumAddressFromPublicKey(bytes _publicKey) public pure returns (address) {
     // 
-    return address(keccak256(lastNBytes(publicKey, 64)));
+    return address(keccak256(lastNBytes(_publicKey, 64)));
   }
 
   /**
    * @notice Generate a Republic ID from an ECDSA public key. It is generated
    * by taking the first 20 bytes of the keccak256 hash of the public key.
    *
-   * @param publicKey The public key.
+   * @param _publicKey The public key.
    *
    * @return A Republic ID.
    */
-  function republicIDFromPublicKey(bytes publicKey) public pure returns (bytes20) {
-    return bytes20(uint(keccak256(publicKey)) >> (8 * 12));
+  function republicIDFromPublicKey(bytes _publicKey) public pure returns (bytes20) {
+    return bytes20(uint(keccak256(_publicKey)) >> (8 * 12));
   }
 
   /**
    * @notice The logarithm function, base 2. Posted by Tjaden Hess
    * https://ethereum.stackexchange.com/a/30168.
    * 
-   * @param x The input to the logarithm function.
+   * @param _x The input to the logarithm function.
    *
    * @return The output of the logarithm function.
    */
-  function logtwo(uint x) public pure returns (uint y) {
+  function logtwo(uint _x) public pure returns (uint y) {
     assembly {
-      let arg := x
-      x := sub(x,1)
-      x := or(x, div(x, 0x02))
-      x := or(x, div(x, 0x04))
-      x := or(x, div(x, 0x10))
-      x := or(x, div(x, 0x100))
-      x := or(x, div(x, 0x10000))
-      x := or(x, div(x, 0x100000000))
-      x := or(x, div(x, 0x10000000000000000))
-      x := or(x, div(x, 0x100000000000000000000000000000000))
-      x := add(x, 1)
+      let arg := _x
+      _x := sub(_x,1)
+      _x := or(_x, div(_x, 0x02))
+      _x := or(_x, div(_x, 0x04))
+      _x := or(_x, div(_x, 0x10))
+      _x := or(_x, div(_x, 0x100))
+      _x := or(_x, div(_x, 0x10000))
+      _x := or(_x, div(_x, 0x100000000))
+      _x := or(_x, div(_x, 0x10000000000000000))
+      _x := or(_x, div(_x, 0x100000000000000000000000000000000))
+      _x := add(_x, 1)
       let m := mload(0x40)
       mstore(m,           0xf8f9cbfae6cc78fbefe7cdc3a1793dfcf4f0e8bbd8cec470b6a28a7a5a3e1efd)
       mstore(add(m,0x20), 0xf5ecf1b3e9debc68e1d9cfabc5997135bfb7a7a3938b7b606b5b4b3f2f1f0ffe)
@@ -80,7 +80,7 @@ library Utils {
       mstore(0x40, add(m, 0x100))
       let magic := 0x818283848586878898a8b8c8d8e8f929395969799a9b9d9e9faaeb6bedeeff
       let shift := 0x100000000000000000000000000000000000000000000000000000000000000
-      let a := div(mul(x, magic), shift)
+      let a := div(mul(_x, magic), shift)
       y := div(mload(add(m,sub(255,a))), shift)
       y := add(y, mul(256, gt(arg, 0x8000000000000000000000000000000000000000000000000000000000000000)))
     }
