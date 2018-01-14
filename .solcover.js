6fb3c1bf3fa823b7042c576cc83cf4a56d0cf08c
Noah Ingham <noahingham@protonmail.com>
2018-01-14 18:57:43 +1100

    Fixed output not being printed again

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,6 @@
 module.exports = {
   // testrpcOptions: '-d --gasLimit 0xfffffffffff --accounts 35 --port 8555',
-  testCommand: '$(./ganache 35) && truffle test',
+  testCommand: 'truffle test',
   skipFiles: ['RepublicToken.sol'],
   port: 8545,
   norpc: true,
