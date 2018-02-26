7640e482289b9e249912bca0a6fce71e19c9b200
Noah <noahingham@protonmail.com>
2018-02-26 12:07:50 +1100

    Removed old utils function

--- test-ts/utils.test.ts
+++ test-ts/utils.test.ts
@@ -14,16 +14,5 @@ let utils: any;
 })();
 
 contract("Utils", function () {
-
-  it("implements the natural logarithm", async function () {
-
-    for (let i = 2; i < 100; i++) {
-      const calculated = (await utils.logtwo.call(i));
-      const expected = Math.ceil(Math.log2(i));
-      // assert(calculated.equals(expected), `Expected ${expected} but got ${calculated}`);
-      calculated
-        .should.be.bignumber.equals(expected);
-    }
-
-  });
+  //
 });
