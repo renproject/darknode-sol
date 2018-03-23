c8517c9ad1d98330a57f6ac4f4de997bf62a287f
susruth <susruth@republicprotocol.com>
2018-03-23 15:08:52 +1100

    Updated migrations

--- migrations/3_distribute_ren.js
+++
@@ -1,9 +0,0 @@
-
-const { accounts } = require("../test/_helpers/accounts");
-
-module.exports = function (deployer) {
-  return artifacts.require("RepublicToken").deployed().then((ren) => {
-    console.log("Sharing around REN tokens...");
-    return Promise.all(accounts.map(account => ren.transfer(account.address, 1000000)));
-  });
-};
---
+++ utils/3_distribute_ren.js
@@ -0,0 +1,9 @@
+
+// const { accounts } = require("../test/_helpers/accounts");
+
+// module.exports = function (deployer) {
+//   return artifacts.require("RepublicToken").deployed().then((ren) => {
+//     console.log("Sharing around REN tokens...");
+//     return Promise.all(accounts.map(account => ren.transfer(account.address, 1000000)));
+//   });
+// };
