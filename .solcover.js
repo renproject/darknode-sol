ce9597035f11ca1340ff1b3994a8be24994835ba
susruth <susruth@republicprotocol.com>
2018-03-26 11:49:33 +1100

    Updated solidity coverage

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,6 @@
 module.exports = {
   testrpcOptions: '-d --gasLimit 0xfffffffffff --accounts 35 --port 8555',
-  // skipFiles: ['RepublicToken.sol', 'Ownable.sol', 'DarkNodeRegistrar.sol', 'TraderRegistrar.sol', 'Gateway.sol', 'LinkedList.sol', 'Migrations.sol', 'Utils.sol', 'LinkedListTest.sol'],
-  skipFiles: ['RepublicToken.sol', 'TraderRegistrar.sol', 'migrations/Migrations.sol'],
+  // skipFiles: ['RepublicToken.sol', 'Ownable.sol', 'DarkNodeRegistry.sol', 'TraderRegistry.sol', 'Gateway.sol', 'LinkedList.sol', 'Migrations.sol', 'Utils.sol', 'LinkedListTest.sol'],
+  skipFiles: ['RepublicToken.sol', 'TraderRegistry.sol', 'migrations/Migrations.sol'],
   port: 8555,
 };
