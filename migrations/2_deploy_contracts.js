e6f09fdd45033fc2e6587a3c69a59eea104ee28f
susruth <susruth@republicprotocol.com>
2018-03-26 11:50:22 +1100

    updated migrations file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
 
 // var RepublicToken = artifacts.require("RepublicToken.sol");
-// var DarkNodeRegistrar = artifacts.require("DarkNodeRegistrar.sol");
-// var TraderRegistrar = artifacts.require("TraderRegistrar.sol");
+// var DarkNodeRegistry = artifacts.require("DarkNodeRegistry.sol");
+// var TraderRegistry = artifacts.require("TraderRegistry.sol");
 var Gateway = artifacts.require("Gateway.sol");
 var LinkedList = artifacts.require("LinkedListTest.sol");
 // var Utils = artifacts.require("Utils.sol");
@@ -9,13 +9,13 @@ var Config = require("../republic-config");
 
 module.exports = function (deployer) {
   // deployer.deploy(Utils);
-  // deployer.link(Utils, [DarkNodeRegistrar, TraderRegistrar]);
+  // deployer.link(Utils, [DarkNodeRegistry, TraderRegistry]);
   deployer.deploy(Gateway, "0x261c74f7dd1ed6a069e18375ab2bee9afcb10956", "0x42a990655bffe188c9823a2f914641a32dcbb1b2", "0x8c98238c9823842a99018a2f914641a32dcbb1b2", 5);
   deployer.deploy(LinkedList);
   // return deployer.deploy(RepublicToken).then(() => {
-  //   deployer.link(RepublicToken, [DarkNodeRegistrar, TraderRegistrar]);
-  //   return deployer.deploy(DarkNodeRegistrar, RepublicToken.address, Config.bondMinimum, Config.epochInterval).then(() => {
-  //     return deployer.deploy(TraderRegistrar, RepublicToken.address, Config.bondMinimum)
+  //   deployer.link(RepublicToken, [DarkNodeRegistry, TraderRegistry]);
+  //   return deployer.deploy(DarkNodeRegistry, RepublicToken.address, Config.bondMinimum, Config.epochInterval).then(() => {
+  //     return deployer.deploy(TraderRegistry, RepublicToken.address, Config.bondMinimum)
   //   })
   // });
 };
