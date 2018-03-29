087a00dfcf25a7f44c05eea5faeaf5e24b94a70f
susruth <susruth@republicprotocol.com>
2018-03-29 16:52:01 +1100

    Commented out libraries not being used

--- contracts/libraries/Utils.sol
+++ contracts/libraries/Utils.sol
@@ -2,13 +2,13 @@ pragma solidity ^0.4.19;
 
 library Utils {
 
-  function toBytes32(bytes data, uint pos) internal pure returns (bytes32) {
-    uint256 subdata = 0;
-    for (uint256 i = 0; i < 32; i++) {
-      subdata += uint256(data[31 + pos - i]) << 8*i;
-    }
-    return bytes32(subdata);
-  }
+  // function toBytes32(bytes data, uint pos) internal pure returns (bytes32) {
+  //   uint256 subdata = 0;
+  //   for (uint256 i = 0; i < 32; i++) {
+  //     subdata += uint256(data[31 + pos - i]) << 8*i;
+  //   }
+  //   return bytes32(subdata);
+  // }
 
 
 }
