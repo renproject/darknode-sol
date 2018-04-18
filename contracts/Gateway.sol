b4a757f22dfc2dfa5375d0afc6f1ce486820ee04
Noah <noahingham@protonmail.com>
2018-04-18 11:56:10 +1000

    Formatted .sol files

--- contracts/Gateway.sol
+++ contracts/Gateway.sol
@@ -1,41 +1,41 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.21;
 
 import "./Ownable.sol";
 
 contract Gateway is Ownable {
 
-  address public republicToken;
-  address public darkNodeRegistry;
-  address public traderRegistry;
-  uint256 public minimumDarkPoolSize;
+    address public republicToken;
+    address public darkNodeRegistry;
+    address public traderRegistry;
+    uint256 public minimumDarkPoolSize;
 
-  event update(bytes4 functionID, uint256 oldValue, uint256 newValue);
+    event update(bytes4 functionID, uint256 oldValue, uint256 newValue);
 
-  function Gateway
-  (   address _republicToken, 
-      address _darkNodeRegistry, 
-      address _traderRegistry,
-      uint256 _minimumDarkPoolSize
-  ) public {
-    republicToken = _republicToken;
-    darkNodeRegistry = _darkNodeRegistry;
-    traderRegistry = _traderRegistry;
-    minimumDarkPoolSize = _minimumDarkPoolSize;
-  }
- 
-  function updateDarkNodeRegistry(address _darkNodeRegistry) public onlyOwner {
-    update(bytes4(sha256("updateDarkNodeRegistry(address)")), uint256(darkNodeRegistry), uint256(_darkNodeRegistry));
-    darkNodeRegistry = _darkNodeRegistry;
-  }
+    function Gateway(
+        address _republicToken, 
+        address _darkNodeRegistry, 
+        address _traderRegistry,
+        uint256 _minimumDarkPoolSize
+    ) public {
+        republicToken = _republicToken;
+        darkNodeRegistry = _darkNodeRegistry;
+        traderRegistry = _traderRegistry;
+        minimumDarkPoolSize = _minimumDarkPoolSize;
+    }
+  
+    function updateDarkNodeRegistry(address _darkNodeRegistry) public onlyOwner {
+        emit update(bytes4(sha256("updateDarkNodeRegistry(address)")), uint256(darkNodeRegistry), uint256(_darkNodeRegistry));
+        darkNodeRegistry = _darkNodeRegistry;
+    }
 
-  function updateTraderRegistry(address _traderRegistry) public onlyOwner {
-    update(bytes4(sha256("updateTraderRegistry(address)")), uint256(traderRegistry), uint256(_traderRegistry));
-    traderRegistry = _traderRegistry;
-  }
+    function updateTraderRegistry(address _traderRegistry) public onlyOwner {
+        emit update(bytes4(sha256("updateTraderRegistry(address)")), uint256(traderRegistry), uint256(_traderRegistry));
+        traderRegistry = _traderRegistry;
+    }
 
-  function updateMinimumDarkPoolSize(uint256 _minimumDarkPoolSize) public onlyOwner {
-    update(bytes4(sha256("updateMinimumDarkPoolSize(uint256)")), minimumDarkPoolSize, _minimumDarkPoolSize);
-    minimumDarkPoolSize = _minimumDarkPoolSize;
-  }
+    function updateMinimumDarkPoolSize(uint256 _minimumDarkPoolSize) public onlyOwner {
+        emit update(bytes4(sha256("updateMinimumDarkPoolSize(uint256)")), minimumDarkPoolSize, _minimumDarkPoolSize);
+        minimumDarkPoolSize = _minimumDarkPoolSize;
+    }
  
 }
