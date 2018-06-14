0a2f0df7baf80e0c2ed6791781fc5a7dc32d90a7
Noah <noahingham@protonmail.com>
2018-06-14 11:04:20 +1000

    Fixed RenExSettlement deployment

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -22,19 +22,19 @@ const CONFIG = {
         minimumBond: 0, // in airen
         minimumPoolSize: 5,
         minumumEpochInterval: 1, // in seconds,
-        address: "",
+        address: "0x9979cd31b0de1f495a4f53d64039e5f69ff06e15",
     },
     Ledger: {
-        address: "" // KOVAN
+        address: "0x51dE68D7767DBa16A21ADE748849e67E765e063C" // KOVAN
     },
     RenExTokens: {
-        address: "",
+        address: "0x45bca0ddc49415c45ac8d3cdc0b793e0c3324f25",
     },
     RenExBalances: {
-        address: "",
+        address: "0x5bc537A67F782ae0ae154B6fcB6dB6ebE1B77313",
     },
     RenExSettlement: {
-        address: "",
+        address: "0xdebe9b6e1f4b29ffa472694ad1befc5309e72f89",
     },
 };
 
@@ -80,21 +80,25 @@ async function deployContract(deployer, artifact) {
 }
 
 module.exports = async function (deployer) {
-    const dnr = await deployDarknodeRegistry(deployer);
-    // const dnr = CONFIG.DNR.address;
-    const ledger = await deployRenLedger(deployer, dnr.address);
-    // const ledger = CONFIG.Ledger.address;
+    // const dnr = await deployDarknodeRegistry(deployer);
+    const dnr = DarknodeRegistry.at(CONFIG.DNR.address);
+    // const ledger = await deployRenLedger(deployer, dnr.address);
+    const ledger = RenLedger.at(CONFIG.Ledger.address);
 
-    // // RENEXTOKENS
-    const renExTokens = await deployContract(deployer, RenExTokens);
-    await renExTokens.registerToken(1, 0x0, 18);
-    await renExTokens.registerToken(0x100, CONFIG.DGX.address, 9);
-    await renExTokens.registerToken(0x10000, CONFIG.REN.address, 18);
+    // // // RENEXTOKENS
+    // const renExTokens = await deployContract(deployer, RenExTokens);
+    // await renExTokens.registerToken(1, 0x0, 18);
+    // await renExTokens.registerToken(0x100, CONFIG.DGX.address, 9);
+    // await renExTokens.registerToken(0x10000, CONFIG.REN.address, 18);
 
-    const renExBalances = await deployContract(deployer, RenExBalances);
+    // const renExBalances = await deployContract(deployer, RenExBalances);
 
-    // const renExTokens = CONFIG.RenExTokens.address;
-    // const renExBalances = RenExBalances.at(CONFIG.RenExBalances.address);
+    const renExTokens = RenExTokens.at(CONFIG.RenExTokens.address);
+    const renExBalances = RenExBalances.at(CONFIG.RenExBalances.address);
+
+    console.log(ledger.address);
+    console.log(renExTokens.address);
+    console.log(renExBalances.address);
 
     const renExSettlement = await deployRenExSettlement(deployer, ledger.address, renExTokens.address, renExBalances.address);
     // const renExSettlement = CONFIG.RenExSettlement.address;
