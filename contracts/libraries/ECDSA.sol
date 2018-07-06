41218f4e48a75367768906c14e267dd562704ca9
negaex <gh@negaex.com>
2018-07-06 10:58:56 +1000

    contracts: Improved test coverage

--- contracts/libraries/ECDSA.sol
+++ contracts/libraries/ECDSA.sol
@@ -3,35 +3,6 @@ pragma solidity ^0.4.24;
 import "./Utils.sol";
 
 library ECDSA {
-
-    /**
-     * @notice Converts a number to its string/bytes representation
-     *
-     * @param _v the uint to convert
-     */
-    function uintToBytes(uint _v) public pure returns (bytes) {
-        uint v = _v;
-        if (v == 0) {
-            return "0";
-        }
-
-        uint digits = 0;
-        uint v2 = v;
-        while (v2 > 0) {
-            v2 /= 10;
-            digits++;
-        }
-
-        bytes memory result = new bytes(digits);
-
-        for (uint i = 0; i < digits; i++) {
-            result[digits - i - 1] = bytes1((v % 10) + 48);
-            v /= 10;
-        }
-
-        return result;
-    }
-
     /**
      * @notice Retrieves the address from a signature
      *
@@ -40,7 +11,7 @@ library ECDSA {
      */
     function addr(bytes _hash, bytes _signature) internal pure returns (address) {
         bytes memory prefix = "\x19Ethereum Signed Message:\n";
-        bytes memory encoded = abi.encodePacked(prefix, uintToBytes(_hash.length), _hash);
+        bytes memory encoded = abi.encodePacked(prefix, Utils.uintToBytes(_hash.length), _hash);
         bytes32 prefixedHash = keccak256(encoded);
 
         // v is expected to be 27 or 28 but some libraries use 0 and 1 instead
--- contracts/libraries/Utils.sol
+++ contracts/libraries/Utils.sol
@@ -2,20 +2,40 @@ pragma solidity ^0.4.24;
 
 library Utils {
 
+    /**
+     * @notice Converts a number to its string/bytes representation
+     *
+     * @param _v the uint to convert
+     */
+    function uintToBytes(uint _v) internal pure returns (bytes) {
+        uint v = _v;
+        if (v == 0) {
+            return "0";
+        }
+
+        uint digits = 0;
+        uint v2 = v;
+        while (v2 > 0) {
+            v2 /= 10;
+            digits++;
+        }
+
+        bytes memory result = new bytes(digits);
+
+        for (uint i = 0; i < digits; i++) {
+            result[digits - i - 1] = bytes1((v % 10) + 48);
+            v /= 10;
+        }
+
+        return result;
+    }
+
     function toBytes32(bytes data, uint pos) internal pure returns (bytes32) {
         uint256 subdata = 0;
         for (uint256 i = 0; i < 32; i++) {
             subdata += uint256(data[31 + pos - i]) << 8*i;
         }
         return bytes32(subdata);
-    }
-
-    function toBytesI(bytes data, uint length) internal pure returns (bytes) {
-        bytes memory result = new bytes(length);
-        for (uint i = 0; i < length; i++) {
-            result[i] = data[i];
-        }
-        return result;
     }
 
     // /**
