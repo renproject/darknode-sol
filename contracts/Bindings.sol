98eb4a16ecd6dfbe7261d73931a5f2a4bf08cc39
negaex <gh@negaex.com>
2018-08-22 14:04:51 +1000

    test-ts: Removed ts bindings from git

--- contracts/Bindings.sol
+++ contracts/Bindings.sol
@@ -6,6 +6,8 @@ import "./DarknodeSlasher.sol";
 import "./Orderbook.sol";
 import "./RepublicToken.sol";
 import "./Settlement.sol";
+import "./SettlementRegistry.sol";
+import "./BrokerVerifier.sol";
 
 /// @notice Bindings imports all of the contracts for generating bindings.
 /* solium-disable no-empty-blocks */
--- contracts/BrokerVerifier.sol
+++ contracts/BrokerVerifier.sol
@@ -3,6 +3,13 @@ pragma solidity ^0.4.24;
 /// @notice The BrokerVerifier interface defines the functions that a settlement
 /// layer's broker verifier contract must implement.
 interface BrokerVerifier {
+
+    /// @notice The function signature that will be called when a trader opens
+    /// an order.
+    ///
+    /// @param _trader The trader requesting the withdrawal.
+    /// @param _signature The 65-byte signature from the broker.
+    /// @param _orderID The 32-byte order ID.
     function verifyOpenSignature(
         address _trader,
         bytes _signature,
