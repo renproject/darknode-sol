ed583df433aa06c825bae6b8232724571397755a
Negaex <gh@negaex.com>
2018-10-26 16:31:07 +1100

    Removed openzeppelin-solidity

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,5 @@
 module.exports = {
-    copyPackages: ["openzeppelin-solidity"], // needed to import from node_modules
+    copyPackages: ["openzeppelin-eth", "zos-lib"], // needed to import from node_modules
     testrpcOptions: "-d --accounts 10 --port 8555",
     skipFiles: [
         // REN token
--- generateGoBindings.sh
+++ generateGoBindings.sh
@@ -1,19 +1,30 @@
 #!/bin/sh
 set -e
 
-# Setup
-sed -i.bak -e 's/"openzeppelin-solidity\/contracts\//".\/openzeppelin-solidity\/contracts\//' contracts/*.sol
-sed -i.bak -e 's/"openzeppelin-solidity\/contracts\//"..\/openzeppelin-solidity\/contracts\//' contracts/*/*.sol
-mkdir ./contracts/openzeppelin-solidity
-cp -r ./node_modules/openzeppelin-solidity/contracts ./contracts/openzeppelin-solidity/contracts
+# Setup openzeppelin-eth
+sed -i.bak -e 's/"openzeppelin-eth\/contracts\//".\/openzeppelin-eth\/contracts\//' contracts/*.sol
+sed -i.bak -e 's/"openzeppelin-eth\/contracts\//"..\/openzeppelin-eth\/contracts\//' contracts/*/*.sol
+mkdir ./contracts/openzeppelin-eth
+cp -r ./node_modules/openzeppelin-eth/contracts ./contracts/openzeppelin-eth/contracts
+
+# Setup zos-lib
+sed -i.bak -e 's/"zos-lib\/contracts\//".\/zos-lib\/contracts\//' contracts/*.sol
+sed -i.bak -e 's/"zos-lib\/contracts\//"..\/zos-lib\/contracts\//' contracts/*/*.sol
+mkdir ./contracts/zos-lib
+cp -r ./node_modules/zos-lib/contracts ./contracts/zos-lib/contracts
 
 ### GENERATE BINDINGS HERE ###
 abigen --sol ./contracts/Bindings.sol -pkg bindings --out bindings.go
 
-# Revert setup
-sed -i.bak -e 's/".\/openzeppelin-solidity\/contracts\//"openzeppelin-solidity\/contracts\//' contracts/*.sol
-sed -i.bak -e 's/"..\/openzeppelin-solidity\/contracts\//"openzeppelin-solidity\/contracts\//' contracts/*/*.sol
-rm -r ./contracts/openzeppelin-solidity
+# Revert setup for openzeppelin-eth
+sed -i.bak -e 's/".\/openzeppelin-eth\/contracts\//"openzeppelin-eth\/contracts\//' contracts/*.sol
+sed -i.bak -e 's/"..\/openzeppelin-eth\/contracts\//"openzeppelin-eth\/contracts\//' contracts/*/*.sol
+rm -r ./contracts/openzeppelin-eth
+
+# Revert setup for zos-lib
+sed -i.bak -e 's/".\/zos-lib\/contracts\//"zos-lib\/contracts\//' contracts/*.sol
+sed -i.bak -e 's/"..\/zos-lib\/contracts\//"zos-lib\/contracts\//' contracts/*/*.sol
+rm -r ./contracts/zos-lib
 
 rm contracts/*/*.bak
 rm contracts/*.bak
