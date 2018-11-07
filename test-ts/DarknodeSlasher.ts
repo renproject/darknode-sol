f3c0ec5817ceb69746ff487124487fea2e794080
Negaex <gh@negaex.com>
2018-11-08 08:12:54 +1100

    test-ts: Fixed DarknodeSlasher tests

--- test-ts/DarknodeSlasher.ts
+++ test-ts/DarknodeSlasher.ts
@@ -1,85 +1,92 @@
 import BigNumber from "bignumber.js";
 
-import * as testUtils from "../test-ts/helper/testUtils";
-import { MINIMUM_BOND } from "../test-ts/helper/testUtils";
+import * as testUtils from "./helper/testUtils";
+import { MINIMUM_BOND } from "./helper/testUtils";
 
-import { ApprovingBrokerArtifact } from "../test-ts/bindings/approving_broker";
-import { BrokerVerifierContract } from "../test-ts/bindings/broker_verifier";
-import { DarknodeRegistryArtifact, DarknodeRegistryContract } from "../test-ts/bindings/darknode_registry";
-import { DarknodeSlasherArtifact, DarknodeSlasherContract } from "../test-ts/bindings/darknode_slasher";
-import { OrderbookArtifact, OrderbookContract } from "../test-ts/bindings/orderbook";
-import { RepublicTokenArtifact, RepublicTokenContract } from "../test-ts/bindings/republic_token";
-import { SettlementRegistryArtifact, SettlementRegistryContract } from "../test-ts/bindings/settlement_registry";
-import { SettlementUtilsTestArtifact, SettlementUtilsTestContract } from "../test-ts/bindings/settlement_utils_test";
+import { ApprovingBrokerArtifact } from "./bindings/approving_broker";
+import { BrokerVerifierContract } from "./bindings/broker_verifier";
+import { DarknodeRegistryContract } from "./bindings/darknode_registry";
+import { DarknodeSlasherContract } from "./bindings/darknode_slasher";
+import { OrderbookContract } from "./bindings/orderbook";
+import { RepublicTokenContract } from "./bindings/republic_token";
+import { SettlementRegistryContract } from "./bindings/settlement_registry";
+import { SettlementUtilsTestArtifact, SettlementUtilsTestContract } from "./bindings/settlement_utils_test";
 
-const SettlementUtilsTest = artifacts.require("SettlementUtilsTest") as SettlementUtilsTestArtifact;
-const RepublicToken = artifacts.require("RepublicToken") as RepublicTokenArtifact;
-const DarknodeRegistry = artifacts.require("DarknodeRegistry") as DarknodeRegistryArtifact;
-const Orderbook = artifacts.require("Orderbook") as OrderbookArtifact;
-const DarknodeSlasher = artifacts.require("DarknodeSlasher") as DarknodeSlasherArtifact;
-const SettlementRegistry = artifacts.require("SettlementRegistry") as SettlementRegistryArtifact;
+import { TestHelper } from "zos";
+
+import * as deployRepublicProtocolContracts from "../migrations/deploy";
+
+const fixWeb3 = require("../migrations/fixWeb3");
+const defaultConfig = require("../migrations/config");
+
 const ApprovingBroker = artifacts.require("ApprovingBroker") as ApprovingBrokerArtifact;
+const SettlementUtilsTest = artifacts.require("SettlementUtilsTest") as SettlementUtilsTestArtifact;
 
 contract("Darknode Slasher", (accounts: string[]) => {
+    const proxyOwner = accounts[9];
+    const contractOwner = accounts[8];
+    // const notOwner = accounts[7];
 
-    let dnr: DarknodeRegistryContract;
-    let ren: RepublicTokenContract;
+    let republicToken: RepublicTokenContract;
+    let darknodeRegistry: DarknodeRegistryContract;
     let orderbook: OrderbookContract;
-    let slasher: DarknodeSlasherContract;
+    let darknodeSlasher: DarknodeSlasherContract;
     let settlementTest: SettlementUtilsTestContract;
+    let settlementRegistry: SettlementRegistryContract;
     const [darknode5, darknode6, darknode7] = [accounts[5], accounts[6], accounts[7]];
 
     const approvingBrokerID = 0x539;
 
     before(async () => {
-        settlementTest = await SettlementUtilsTest.new();
+        fixWeb3(web3, artifacts);
+        this.app = await TestHelper({ from: proxyOwner, gasPrice: 10000000000 });
+        const config = { ...defaultConfig, CONTRACT_OWNER: contractOwner };
+        ({ orderbook, darknodeRegistry, republicToken, darknodeSlasher, settlementRegistry } =
+            await deployRepublicProtocolContracts(artifacts, this.app, config));
 
-        ren = await RepublicToken.deployed();
-        dnr = await DarknodeRegistry.deployed();
-        orderbook = await Orderbook.deployed();
-        slasher = await DarknodeSlasher.deployed();
+        settlementTest = await SettlementUtilsTest.new({ from: contractOwner });
 
-        const settlementRegistry: SettlementRegistryContract = await SettlementRegistry.deployed();
+        const approvingBroker: BrokerVerifierContract = await ApprovingBroker.new({ from: contractOwner });
 
-        const approvingBroker: BrokerVerifierContract = await ApprovingBroker.new();
-
-        await settlementRegistry.registerSettlement(approvingBrokerID, testUtils.NULL, approvingBroker.address);
+        await settlementRegistry.registerSettlement(
+            approvingBrokerID, testUtils.NULL, approvingBroker.address, { from: contractOwner },
+        );
 
         // Register 3 darknodes
-        await ren.transfer(accounts[1], MINIMUM_BOND.toFixed());
-        await ren.transfer(accounts[2], MINIMUM_BOND.toFixed());
-        await ren.transfer(accounts[3], MINIMUM_BOND.toFixed());
+        await republicToken.transfer(accounts[1], MINIMUM_BOND.toFixed(), { from: contractOwner });
+        await republicToken.transfer(accounts[2], MINIMUM_BOND.toFixed(), { from: contractOwner });
+        await republicToken.transfer(accounts[3], MINIMUM_BOND.toFixed(), { from: contractOwner });
 
-        await ren.approve(dnr.address, MINIMUM_BOND.toFixed(), { from: accounts[1] });
-        await ren.approve(dnr.address, MINIMUM_BOND.toFixed(), { from: accounts[2] });
-        await ren.approve(dnr.address, MINIMUM_BOND.toFixed(), { from: accounts[3] });
+        await republicToken.approve(darknodeRegistry.address, MINIMUM_BOND.toFixed(), { from: accounts[1] });
+        await republicToken.approve(darknodeRegistry.address, MINIMUM_BOND.toFixed(), { from: accounts[2] });
+        await republicToken.approve(darknodeRegistry.address, MINIMUM_BOND.toFixed(), { from: accounts[3] });
 
-        await dnr.register(darknode5, testUtils.PUBK("1"), { from: accounts[1] });
-        await dnr.register(darknode6, testUtils.PUBK("2"), { from: accounts[2] });
-        await dnr.register(darknode7, testUtils.PUBK("3"), { from: accounts[3] });
-        await testUtils.waitForEpoch(dnr);
+        await darknodeRegistry.register(darknode5, testUtils.PUBK("1"), { from: accounts[1] });
+        await darknodeRegistry.register(darknode6, testUtils.PUBK("2"), { from: accounts[2] });
+        await darknodeRegistry.register(darknode7, testUtils.PUBK("3"), { from: accounts[3] });
+        await testUtils.waitForEpoch(darknodeRegistry, { from: contractOwner });
 
-        (await dnr.isRegistered(darknode5)).should.be.true;
-        (await dnr.isRegistered(darknode6)).should.be.true;
-        (await dnr.isRegistered(darknode7)).should.be.true;
+        (await darknodeRegistry.isRegistered(darknode5)).should.be.true;
+        (await darknodeRegistry.isRegistered(darknode6)).should.be.true;
+        (await darknodeRegistry.isRegistered(darknode7)).should.be.true;
 
-        (await dnr.isDeregisterable(darknode5)).should.be.true;
-        (await dnr.isDeregisterable(darknode6)).should.be.true;
-        (await dnr.isDeregisterable(darknode7)).should.be.true;
+        (await darknodeRegistry.isDeregisterable(darknode5)).should.be.true;
+        (await darknodeRegistry.isDeregisterable(darknode6)).should.be.true;
+        (await darknodeRegistry.isDeregisterable(darknode7)).should.be.true;
     });
 
     it("anyone other than registered darknodes cannot submit challenge order", async () => {
         const ORDER = [web3.utils.sha3("1"), 1, "0x100000000", 10, 1000, 0];
 
-        await slasher.submitChallengeOrder.apply(this, [...ORDER, { from: accounts[1] }])
+        await darknodeSlasher.submitChallengeOrder.apply(this, [...ORDER, { from: accounts[1] }])
             .should.be.rejectedWith(null, /must be darknode/);
     });
 
     it("should fail to submit challenge order twice", async () => {
         const ORDER = [web3.utils.sha3("2"), 1, "0x100000000", 10, 1000, 0];
-        await slasher.submitChallengeOrder.apply(this, [...ORDER, { from: darknode5 }]);
+        await darknodeSlasher.submitChallengeOrder.apply(this, [...ORDER, { from: darknode5 }]);
 
-        await slasher.submitChallengeOrder.apply(this, [...ORDER, { from: darknode5 }])
+        await darknodeSlasher.submitChallengeOrder.apply(this, [...ORDER, { from: darknode5 }])
             .should.be.rejectedWith(null, /already submitted/);
     });
 
@@ -87,20 +94,20 @@ contract("Darknode Slasher", (accounts: string[]) => {
         const BUY = [web3.utils.sha3("3"), 2, "0x100000000", 10, 1, 2];
         const SELL = [web3.utils.sha3("4"), 2, "0x1", 10, 1, 2];
 
-        await slasher.submitChallengeOrder.apply(this, [...BUY, { from: darknode5 }]);
-        await slasher.submitChallengeOrder.apply(this, [...SELL, { from: darknode6 }]);
+        await darknodeSlasher.submitChallengeOrder.apply(this, [...BUY, { from: darknode5 }]);
+        await darknodeSlasher.submitChallengeOrder.apply(this, [...SELL, { from: darknode6 }]);
 
         const buyID = await settlementTest.hashOrder.apply(this, [...BUY]);
         const sellID = await settlementTest.hashOrder.apply(this, [...SELL]);
 
-        await testUtils.openOrder(orderbook, approvingBrokerID, accounts[8], buyID);
-        await testUtils.openOrder(orderbook, approvingBrokerID, accounts[9], sellID);
+        await testUtils.openBuyOrder(orderbook, approvingBrokerID, accounts[8], buyID);
+        await testUtils.openSellOrder(orderbook, approvingBrokerID, accounts[4], sellID);
         await orderbook.confirmOrder(buyID, sellID, { from: darknode7 });
 
         // The confirmer's bond will be halved
-        const bondBefore = await dnr.getDarknodeBond(darknode7);
-        await slasher.submitChallenge(buyID, sellID);
-        const bondAfter = new BigNumber((await dnr.getDarknodeBond(darknode7)));
+        const bondBefore = await darknodeRegistry.getDarknodeBond(darknode7);
+        await darknodeSlasher.submitChallenge(buyID, sellID, { from: accounts[0] });
+        const bondAfter = new BigNumber((await darknodeRegistry.getDarknodeBond(darknode7)));
         bondAfter.times(2).should.bignumber.equal(bondBefore);
     });
 
@@ -108,22 +115,22 @@ contract("Darknode Slasher", (accounts: string[]) => {
         const BUY = [web3.utils.sha3("5"), 2, "0x100000000", 10, 1, 2];
         const SELL = [web3.utils.sha3("6"), 2, "0x1", 10, 1, 2];
 
-        await slasher.submitChallengeOrder.apply(this, [...BUY, { from: darknode5 }]);
-        await slasher.submitChallengeOrder.apply(this, [...SELL, { from: darknode6 }]);
+        await darknodeSlasher.submitChallengeOrder.apply(this, [...BUY, { from: darknode5 }]);
+        await darknodeSlasher.submitChallengeOrder.apply(this, [...SELL, { from: darknode6 }]);
 
         const buyID = await settlementTest.hashOrder.apply(this, [...BUY]);
         const sellID = await settlementTest.hashOrder.apply(this, [...SELL]);
 
-        await testUtils.openOrder(orderbook, approvingBrokerID, accounts[8], buyID);
-        await testUtils.openOrder(orderbook, approvingBrokerID, accounts[9], sellID);
+        await testUtils.openBuyOrder(orderbook, approvingBrokerID, accounts[8], buyID);
+        await testUtils.openSellOrder(orderbook, approvingBrokerID, accounts[4], sellID);
         await orderbook.confirmOrder(buyID, sellID, { from: darknode7 });
 
-        await slasher.submitChallenge(buyID, sellID);
+        await darknodeSlasher.submitChallenge(buyID, sellID, { from: accounts[0] });
 
         // Slashing (and with orders swapped) should be rejected
-        await slasher.submitChallenge(buyID, sellID)
+        await darknodeSlasher.submitChallenge(buyID, sellID, { from: accounts[0] })
             .should.be.rejectedWith(/already challenged/);
-        await slasher.submitChallenge(sellID, buyID)
+        await darknodeSlasher.submitChallenge(sellID, buyID, { from: accounts[0] })
             .should.be.rejectedWith(/already challenged/);
     });
 
@@ -131,17 +138,17 @@ contract("Darknode Slasher", (accounts: string[]) => {
         const BUY = [web3.utils.sha3("6"), 1, "0x100000000", 10, 1000, 0];
         const SELL = [web3.utils.sha3("7"), 1, "0x1", 10, 10000, 0];
 
-        await slasher.submitChallengeOrder.apply(this, [...BUY, { from: darknode5 }]);
-        await slasher.submitChallengeOrder.apply(this, [...SELL, { from: darknode6 }]);
+        await darknodeSlasher.submitChallengeOrder.apply(this, [...BUY, { from: darknode5 }]);
+        await darknodeSlasher.submitChallengeOrder.apply(this, [...SELL, { from: darknode6 }]);
 
         const sellID = await settlementTest.hashOrder.apply(this, [...BUY]);
         const buyID = await settlementTest.hashOrder.apply(this, [...SELL]);
-        await testUtils.openOrder(orderbook, approvingBrokerID, accounts[8], buyID);
-        await testUtils.openOrder(orderbook, approvingBrokerID, accounts[9], sellID);
+        await testUtils.openBuyOrder(orderbook, approvingBrokerID, accounts[8], buyID);
+        await testUtils.openSellOrder(orderbook, approvingBrokerID, accounts[4], sellID);
         await orderbook.confirmOrder(buyID, sellID, { from: darknode7 });
 
         // Slash should be rejected
-        await slasher.submitChallenge(buyID, sellID)
+        await darknodeSlasher.submitChallenge(buyID, sellID, { from: accounts[0] })
             .should.be.rejectedWith(/invalid challenge/);
     });
 
@@ -149,16 +156,16 @@ contract("Darknode Slasher", (accounts: string[]) => {
         const BUY = [web3.utils.sha3("7"), 1, "0x100000000", 1, 1, 0];
         const SELL = [web3.utils.sha3("8"), 1, "0x1", 1, 1, 0];
 
-        await slasher.submitChallengeOrder.apply(this, [...BUY, { from: darknode5 }]);
-        await slasher.submitChallengeOrder.apply(this, [...SELL, { from: darknode6 }]);
+        await darknodeSlasher.submitChallengeOrder.apply(this, [...BUY, { from: darknode5 }]);
+        await darknodeSlasher.submitChallengeOrder.apply(this, [...SELL, { from: darknode6 }]);
 
         const sellID = await settlementTest.hashOrder.apply(this, [...BUY]);
         const buyID = await settlementTest.hashOrder.apply(this, [...SELL]);
-        await testUtils.openOrder(orderbook, approvingBrokerID, accounts[8], buyID);
-        await testUtils.openOrder(orderbook, approvingBrokerID, accounts[9], sellID);
+        await testUtils.openBuyOrder(orderbook, approvingBrokerID, accounts[8], buyID);
+        await testUtils.openSellOrder(orderbook, approvingBrokerID, accounts[4], sellID);
 
         // Slash should be rejected
-        await slasher.submitChallenge(buyID, sellID)
+        await darknodeSlasher.submitChallenge(buyID, sellID, { from: accounts[0] })
             .should.be.rejectedWith(/unconfirmed orders/);
     });
 
@@ -169,14 +176,14 @@ contract("Darknode Slasher", (accounts: string[]) => {
         const buyID = await settlementTest.hashOrder.apply(this, [...BUY]);
         const sellID = await settlementTest.hashOrder.apply(this, [...SELL]);
 
-        // Slash should be rejected if buy details aren't available
-        await slasher.submitChallenge(buyID, sellID)
+        // Slash should be rejected if buy details arepublicToken't available
+        await darknodeSlasher.submitChallenge(buyID, sellID, { from: accounts[0] })
             .should.be.rejectedWith(/details unavailable/);
 
-        await slasher.submitChallengeOrder.apply(this, [...BUY, { from: darknode5 }]);
+        await darknodeSlasher.submitChallengeOrder.apply(this, [...BUY, { from: darknode5 }]);
 
-        // Slash should be rejected if sell details aren't available
-        await slasher.submitChallenge(buyID, sellID)
+        // Slash should be rejected if sell details arepublicToken't available
+        await darknodeSlasher.submitChallenge(buyID, sellID, { from: accounts[0] })
             .should.be.rejectedWith(/details unavailable/);
     });
 });
