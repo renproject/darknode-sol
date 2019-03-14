2fedc043619336eed3f09a11cc892fca305641cc
Vince Au <vinceau09@gmail.com>
2019-03-15 10:55:27 +1100

    test-ts: Update tests for DarknodePayment

--- test-ts/DarknodePayment.ts
+++ test-ts/DarknodePayment.ts
@@ -22,6 +22,8 @@ contract("DarknodePayment", (accounts: string[]) => {
     let dnr: DarknodeRegistryContract;
     let ren: RepublicTokenContract;
 
+    const darknode1 = accounts[1];
+
     before(async () => {
         ren = await RepublicToken.deployed();
         dai = await ERC20.deployed();
@@ -29,7 +31,8 @@ contract("DarknodePayment", (accounts: string[]) => {
         dnp = await DarknodePayment.deployed();
 
         // [ACTION] Register
-        for (let i = 0; i < accounts.length; i++) {
+        // Don't register a darknode under account[0]
+        for (let i = 1; i < accounts.length; i++) {
             await ren.transfer(accounts[i], MINIMUM_BOND);
             await ren.approve(dnr.address, MINIMUM_BOND, { from: accounts[i] });
             // Register the darknodes under the account address
@@ -47,29 +50,51 @@ contract("DarknodePayment", (accounts: string[]) => {
     });
 
     it("can be paid DAI from a payee", async () => {
-        const previousBalance = new BN(await dnp.balance());
+        const previousBalance = new BN(await dnp.currentEpochRewardPool());
+        previousBalance.should.bignumber.equal(new BN(0));
 
         // Approve the contract to use DAI
         const amount = new BN("100000000000000000");
         await dai.approve(dnp.address, amount);
         await dnp.deposit(amount);
+        const newRewardPool = previousBalance.add(amount);
         // We should expect the DAI balance to have increased by what we deposited
-        (await dnp.balance()).should.bignumber.equal(previousBalance.add(amount));
+        (await dnp.currentEpochRewardPool()).should.bignumber.equal(newRewardPool);
     });
 
     it("cannot withdraw if there is no balance", async () => {
-        await dnp.withdraw().should.be.rejectedWith(null, /nothing to withdraw/);
+        await dnp.withdraw({ from: darknode1 }).should.be.rejectedWith(null, /nothing to withdraw/);
     })
 
     it("cannot call tick twice in the same epoch", async () => {
-        await dnp.tick();
-        await dnp.tick().should.be.rejectedWith(null, /already ticked/);
+        await dnp.tick({ from: darknode1 });
+        await dnp.tick({ from: darknode1 }).should.be.rejectedWith(null, /already ticked/);
     })
 
     it("can tick again after an epoch has passed", async () => {
-        await dnp.tick();
+        await dnp.tick({ from: darknode1 });
         await waitForEpoch(dnr);
-        await dnp.tick().should.not.be.rejectedWith(null, /already ticked/);
+        await dnp.tick({ from: darknode1 }).should.not.be.rejectedWith(null, /already ticked/);
+    })
+
+    it("can withdraw DAI out of contract", async () => {
+        const oldDAIBalance = new BN(await dai.balanceOf(darknode1));
+
+        // Tick twice to allocate rewards
+        await dnp.tick({ from: darknode1 });
+        await waitForEpoch(dnr);
+        await dnp.tick({ from: darknode1 });
+
+        const earnedDAIRewards = new BN(await dnp.darknodeBalances(darknode1));
+        await dnp.withdraw({ from: darknode1 });
+
+        // Our balances should have increased
+        const newDAIBalance = new BN(await dai.balanceOf(darknode1));
+        newDAIBalance.should.bignumber.equal(oldDAIBalance.add(earnedDAIRewards));
+
+        // We should have nothing left to withdraw
+        const postWithdrawRewards = new BN(await dnp.darknodeBalances(darknode1));
+        postWithdrawRewards.should.bignumber.equal(new BN(0));
     })
 
 });
