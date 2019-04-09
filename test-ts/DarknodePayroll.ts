5ae31b0095081f6d48ce23d1251e91da4fe25c19
Vince Au <vinceau09@gmail.com>
2019-04-09 15:26:48 +1000

    test-ts: remove .only

--- test-ts/DarknodePayroll.ts
+++ test-ts/DarknodePayroll.ts
@@ -24,7 +24,7 @@ const day = 24 * hour;
 
 const CYCLE_DURATION = DARKNODE_PAYMENT_CYCLE_DURATION * day;
 
-contract.only("DarknodePayroll", (accounts: string[]) => {
+contract("DarknodePayroll", (accounts: string[]) => {
 
     let dnp: DarknodePaymentContract;
     let dai: ERC20Contract;
