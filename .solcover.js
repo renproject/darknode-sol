08d1402b0e655d831b16884b9819fcc82cfb00c7
Vince Au <vinceau09@gmail.com>
2019-04-12 15:27:07 +1000

    solcover: don't run coverage for PaymentToken

--- .solcover.js
+++ .solcover.js
@@ -24,6 +24,7 @@ module.exports = {
         "test/UtilsTest.sol",
         "test/tokens/TokenWithFees.sol",
         "test/LinkedListTest.sol",
+        "test/tokens/PaymentToken.sol",
         "test/CycleChanger.sol",
     ],
 };
