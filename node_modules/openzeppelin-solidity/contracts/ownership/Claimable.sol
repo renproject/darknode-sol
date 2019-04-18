7ec9a8c115e2129e00cf87eba668f452ef827588
Vince Au <vinceau09@gmail.com>
2019-04-18 17:10:38 +1000

    openzeppelin: add Claimable.sol

---
+++ node_modules/openzeppelin-solidity/contracts/ownership/Claimable.sol
@@ -0,0 +1,81 @@
+pragma solidity ^0.5.2;
+
+/**
+ * @title Claimable
+ * @dev Extension for the Ownable contract, where the ownership needs to be claimed.
+ * This allows the new owner to accept the transfer.
+ */
+contract Claimable {
+    address private _pendingOwner;
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+     * account.
+     */
+    constructor () internal {
+        _owner = msg.sender;
+        emit OwnershipTransferred(address(0), _owner);
+    }
+
+    /**
+     * @return the address of the owner.
+     */
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(isOwner());
+        _;
+    }
+
+    /**
+    * @dev Modifier throws if called by any account other than the pendingOwner.
+    */
+    modifier onlyPendingOwner() {
+      require(msg.sender == _pendingOwner);
+      _;
+    }
+
+    /**
+     * @return true if `msg.sender` is the owner of the contract.
+     */
+    function isOwner() public view returns (bool) {
+        return msg.sender == _owner;
+    }
+
+    /**
+     * @dev Allows the current owner to relinquish control of the contract.
+     * It will not be possible to call the functions with the `onlyOwner`
+     * modifier anymore.
+     * @notice Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    /**
+    * @dev Allows the current owner to set the pendingOwner address.
+    * @param newOwner The address to transfer ownership to.
+    */
+    function transferOwnership(address newOwner) onlyOwner public {
+      _pendingOwner = newOwner;
+    }
+
+    /**
+    * @dev Allows the pendingOwner address to finalize the transfer.
+    */
+    function claimOwnership() onlyPendingOwner public {
+      emit OwnershipTransferred(_owner, _pendingOwner);
+      _owner = _pendingOwner;
+      _pendingOwner = address(0);
+    }
+}
