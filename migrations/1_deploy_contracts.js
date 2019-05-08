2f98f66ed408882370882ed3aaabe106ad0f7fd9
noiach <gh@noiach.com>
2019-05-08 14:56:07 +1000

    Updated migration script

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -18,29 +18,33 @@ module.exports = async function (deployer, network) {
 
     let tokens = new Map();
 
-    if (network.match("kovan")) {
-        RenToken.address = "0x2cd647668494c1b15743ab283a0f980d90a87394";
-        DarknodeSlasher.address = "0x0000000000000000000000000000000000000000";
-        DarknodeRegistry.address = "0x1C6309618338D0EDf9a7Ea8eA18E060fD323020D";
-        DarknodeRegistryStore.address = "0x88e4477e4fdd677aee2dc9376471d45c198669fa";
-        DarknodePaymentStore.address = "0x731Ea4Ba77fF184d89dBeB160A0078274Acbe9D2";
-        DarknodePayment.address = "0x5a7802E66b067cB1770ee5b1165AA201690A8B6a";
-        tokens = new Map()
-            .set("DAI", "0xc4375b7de8af5a38a93548eb8453a498222c4ff2")
-            .set("ETH", "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE")
-        // .set("zBTC", "0x2a8368d2a983a0aeae8da0ebc5b7c03a0ea66b37")
-        // .set("zZEC", "0xd67256552f93b39ac30083b4b679718a061feae6");
-        ;
-    } else if (network.match("mainnet")) {
+    if (network.match("mainnet")) {
+
+        // MAINNET!
         RenToken.address = "0x408e41876cCCDC0F92210600ef50372656052a38";
         DarknodeSlasher.address = "0x0000000000000000000000000000000000000000";
         DarknodeRegistry.address = "0x34bd421C7948Bc16f826Fd99f9B785929b121633";
         DarknodeRegistryStore.address = "0x06df0657ba5e8f5339e742212669f6e7ee3c5057";
-        DarknodePaymentStore.address = "";
-        DarknodePayment.address = "";
+        DarknodePaymentStore.address = "0x731Ea4Ba77fF184d89dBeB160A0078274Acbe9D2";
+        DarknodePayment.address = "0x5a7802E66b067cB1770ee5b1165AA201690A8B6a";
         tokens = new Map()
             .set("DAI", "0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359")
             .set("ETH", "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE");
+
+    } else if (network.match("kovan")) {
+
+        RenToken.address = "0x2cd647668494c1b15743ab283a0f980d90a87394";
+        DarknodeSlasher.address = "0x0000000000000000000000000000000000000000";
+        DarknodeRegistry.address = "0x1C6309618338D0EDf9a7Ea8eA18E060fD323020D";
+        DarknodeRegistryStore.address = "0x88e4477e4fdd677aee2dc9376471d45c198669fa";
+        DarknodePaymentStore.address = "0xA9411C3AD1fBE168fd119A3B32fB481a0b9877A9";
+        DarknodePayment.address = "0x8E11B87547f4072CC8A094F2888201CAF4EA0B9e";
+        tokens = new Map()
+            .set("DAI", "0xc4375b7de8af5a38a93548eb8453a498222c4ff2")
+            .set("ETH", "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE")
+            .set("zBTC", "0x2a8368d2a983a0aeae8da0ebc5b7c03a0ea66b37")
+            .set("zZEC", "0xd67256552f93b39ac30083b4b679718a061feae6");
+
     } else {
         RenToken.address = "";
         DarknodeSlasher.address = "";
@@ -163,7 +167,7 @@ module.exports = async function (deployer, network) {
 
     if (changeCycle) {
         try {
-            deployer.logger.log("Updating cycle length");
+            deployer.logger.log("Attempting to change cycle");
             await darknodePayment.changeCycle();
         } catch (error) {
             console.error("Unable to call darknodePayment.changeCycle()");
