55d3be8be5428dd4793f5e9ed085f36df66c81c0
susruth <susruth@republicprotocol.com>
2019-06-20 10:26:32 +1000

    contracts/Shifter: Add getShifterBySymbol to ShifterRegistry

--- contracts/Shifter/ShifterRegistry.sol
+++ contracts/Shifter/ShifterRegistry.sol
@@ -1,16 +1,21 @@
 pragma solidity ^0.5.8;
 
 import "../libraries/Claimable.sol";
+import "./ERC20Shifted.sol";
 
 contract ShifterRegistry is Claimable {
-    /// @notice A map of tokens to canonical shifter addresses
+    /// @notice A map of token addresses to canonical shifter addresses
     mapping (address=>address) private shifters;
+
+    /// @notice A map of token symbols to canonical shifter addresses
+    mapping (string=>address) private shifterBySymbols;
 
     /// @notice Allow the owner to update shifter address for a given 
     ///         ERC20Shifted token contract.
     /// @param _tokenAddress The address of the ERC20Shifted token contract.
     /// @param _shifterAddress The address of the Shifter contract.
-    function setShifter(address _tokenAddress, address _shifterAddress) external onlyOwner {
+    function setShifter(ERC20Shifted _tokenAddress, address _shifterAddress) external onlyOwner {
+        shifterBySymbols[_tokenAddress.symbol()] = _shifterAddress;
         shifters[_tokenAddress] = _shifterAddress;
     }
 
@@ -19,5 +24,12 @@ contract ShifterRegistry is Claimable {
     /// @param _tokenAddress The address of the ERC20Shifted token contract.
     function getShifter(address _tokenAddress) view external returns (address) {
         return shifters[_tokenAddress];
+    }
+
+    /// @notice Returns the Shifter address for the given ERC20Shifted token
+    ///         symbol.
+    /// @param _tokenAddress The address of the ERC20Shifted token contract.
+    function getShifterBySymbol(string memory _tokenSymbol) view external returns (address) {
+        return shifterBySymbols[_tokenSymbol];
     }
 }
