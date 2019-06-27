822393f1af9ba52ee4163fca0f279f908f49b4c6
susruth <susruth@republicprotocol.com>
2019-06-28 09:51:12 +1000

    contracts/Shifter: add Shifter interface

---
+++ contracts/Shifter/IShifter.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.5.8;
+
+interface IShifter {
+    function shiftIn(bytes32 _pHash, uint256 _amount, bytes32 _nHash, bytes calldata _sig) external returns (uint256);
+    function shiftOut(bytes calldata _to, uint256 _amount) external returns (uint256);
+}
