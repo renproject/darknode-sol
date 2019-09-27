40d097b213f6d6069427119db7f070df35a8fdb5
Vince Au <vinceau09@gmail.com>
2019-09-27 15:20:44 +1000

    test: DarknodeSlasher does not slash when signed by different darknodes

--- test/DarknodeSlasher.ts
+++ test/DarknodeSlasher.ts
@@ -9,39 +9,60 @@ import {
 import { Account } from "web3-eth-accounts";
 import { ecsign } from "ethereumjs-util";
 import { Ox } from "./helper/testUtils";
-// import {
-//     ID, MINIMUM_BOND, MINIMUM_EPOCH_INTERVAL_SECONDS, MINIMUM_POD_SIZE, NULL, PUBK, waitForEpoch,
-// } from "./helper/testUtils";
+import {
+    ID, MINIMUM_BOND, MINIMUM_EPOCH_INTERVAL_SECONDS, MINIMUM_POD_SIZE, NULL, PUBK, waitForEpoch,
+} from "./helper/testUtils";
 
-// const RenToken = artifacts.require("RenToken");
-// const DarknodeRegistryStore = artifacts.require("DarknodeRegistryStore");
-// const DarknodeRegistry = artifacts.require("DarknodeRegistry");
+const RenToken = artifacts.require("RenToken");
+const DarknodeRegistryStore = artifacts.require("DarknodeRegistryStore");
+const DarknodeRegistry = artifacts.require("DarknodeRegistry");
 const DarknodeSlasher = artifacts.require("DarknodeSlasher");
+
+interface Darknode {
+    account: Account;
+    privateKey: Buffer;
+}
+
+const numDarknodes = 2;
 
 contract("DarknodeSlasher", (accounts: string[]) => {
 
-    // let ren: RenTokenInstance;
-    // let dnrs: DarknodeRegistryStoreInstance;
-    // let dnr: DarknodeRegistryInstance;
+    let ren: RenTokenInstance;
+    let dnrs: DarknodeRegistryStoreInstance;
+    let dnr: DarknodeRegistryInstance;
     let slasher: DarknodeSlasherInstance;
-    let darknode1: Account;
-    let privKey: Buffer;
+    let darknodes = new Array<Darknode>();
 
+    const owner = accounts[0];
 
     before(async () => {
-        // ren = await RenToken.deployed();
-        // dnrs = await DarknodeRegistryStore.deployed();
-        // dnr = await DarknodeRegistry.deployed();
+        ren = await RenToken.deployed();
+        dnrs = await DarknodeRegistryStore.deployed();
+        dnr = await DarknodeRegistry.deployed();
         slasher = await DarknodeSlasher.deployed();
-        // await dnr.updateSlasher(slasher.address);
-        // await dnr.epoch({ from: accounts[1] }).should.be.rejectedWith(/not authorized/);
-        // await waitForEpoch(dnr);
+        await dnr.updateSlasher(slasher.address);
+        await waitForEpoch(dnr);
 
-        // for (let i = 1; i < accounts.length; i++) {
-        //     await ren.transfer(accounts[i], MINIMUM_BOND);
-        // }
-        darknode1 = web3.eth.accounts.create();
-        privKey = Buffer.from(darknode1.privateKey.slice(2), "hex");
+        for (let i = 0; i < numDarknodes; i++) {
+            const darknode = web3.eth.accounts.create();
+            const privKey = Buffer.from(darknode.privateKey.slice(2), "hex");
+
+            // top up the darknode address with 1 ETH
+            await web3.eth.sendTransaction({ to: darknode.address, from: owner, value: web3.utils.toWei("1") });
+            await web3.eth.personal.importRawKey(darknode.privateKey, "");
+            await web3.eth.personal.unlockAccount(darknode.address, "", 6000);
+
+            // transfer ren and register darknode
+            await ren.transfer(darknode.address, MINIMUM_BOND);
+            await ren.approve(dnr.address, MINIMUM_BOND, { from: darknode.address });
+            // Register the darknodes under the account address
+            await dnr.register(darknode.address, PUBK(darknode.address), { from: darknode.address });
+            darknodes.push({
+                account: darknode,
+                privateKey: privKey,
+            });
+        }
+        await waitForEpoch(dnr);
     });
 
     describe("when generating messages", async () => {
@@ -61,6 +82,7 @@ contract("DarknodeSlasher", (accounts: string[]) => {
     describe("when handling propose messages", async () => {
 
         it("should recover the signer of a message", async () => {
+            const darknode = darknodes[0];
             const height = new BN("6349374925919561232");
             const round = new BN("3652381888914236532");
             const blockhash = "XTsJ2rO2yD47tg3JfmakVRXLzeou4SMtZvsMc6lkr6o";
@@ -68,13 +90,14 @@ contract("DarknodeSlasher", (accounts: string[]) => {
             const validRound = new BN("6345888412984379713");
             const proposeMsg = generateProposeMessage(height, round, blockhash, validRound);
             const hash = hashjs.sha256().update(proposeMsg).digest('hex')
-            const sig = ecsign(Buffer.from(hash, "hex"), privKey);
+            const sig = ecsign(Buffer.from(hash, "hex"), darknode.privateKey);
             const sigString = Ox(`${sig.r.toString("hex")}${sig.s.toString("hex")}${(sig.v).toString(16)}`);
             const signer = await slasher.recoverPropose(height, round, hexBlockhash, validRound, sigString);
-            signer.should.equal(darknode1.address);
+            signer.should.equal(darknode.account.address);
         });
 
         it("cannot slash when the same data is given twice", async () => {
+            const darknode = darknodes[0];
             const height = new BN("6349374925919561232");
             const round = new BN("3652381888914236532");
             const blockhash1 = "XTsJ2rO2yD47tg3JfmakVRXLzeou4SMtZvsMc6lkr6o";
@@ -82,7 +105,7 @@ contract("DarknodeSlasher", (accounts: string[]) => {
             const validRound1 = new BN("6345888412984379713");
             const proposeMsg1 = generateProposeMessage(height, round, blockhash1, validRound1);
             const hash1 = hashjs.sha256().update(proposeMsg1).digest('hex')
-            const sig1 = ecsign(Buffer.from(hash1, "hex"), privKey);
+            const sig1 = ecsign(Buffer.from(hash1, "hex"), darknode.privateKey);
             const sigString1 = Ox(`${sig1.r.toString("hex")}${sig1.s.toString("hex")}${(sig1.v).toString(16)}`);
 
             await slasher.slashDuplicatePropose(
@@ -97,7 +120,7 @@ contract("DarknodeSlasher", (accounts: string[]) => {
             ).should.eventually.be.rejected;
         });
 
-        it("should slash duplicate proposals for the same height and round", async () => {
+        it("cannot slash when the signers are different", async () => {
             const height = new BN("6349374925919561232");
             const round = new BN("3652381888914236532");
             const blockhash1 = "XTsJ2rO2yD47tg3JfmakVRXLzeou4SMtZvsMc6lkr6o";
@@ -105,7 +128,7 @@ contract("DarknodeSlasher", (accounts: string[]) => {
             const validRound1 = new BN("6345888412984379713");
             const proposeMsg1 = generateProposeMessage(height, round, blockhash1, validRound1);
             const hash1 = hashjs.sha256().update(proposeMsg1).digest('hex')
-            const sig1 = ecsign(Buffer.from(hash1, "hex"), privKey);
+            const sig1 = ecsign(Buffer.from(hash1, "hex"), darknodes[0].privateKey);
             const sigString1 = Ox(`${sig1.r.toString("hex")}${sig1.s.toString("hex")}${(sig1.v).toString(16)}`);
 
             const blockhash2 = "41RLyhshTwmPyAwjPM8AmReOB/q4LLdvYpDMKt1bEFI";
@@ -113,7 +136,40 @@ contract("DarknodeSlasher", (accounts: string[]) => {
             const validRound2 = new BN("5327204637322492082");
             const proposeMsg2 = generateProposeMessage(height, round, blockhash2, validRound2);
             const hash2 = hashjs.sha256().update(proposeMsg2).digest('hex')
-            const sig2 = ecsign(Buffer.from(hash2, "hex"), privKey);
+            const sig2 = ecsign(Buffer.from(hash2, "hex"), darknodes[1].privateKey);
+            const sigString2 = Ox(`${sig2.r.toString("hex")}${sig2.s.toString("hex")}${(sig2.v).toString(16)}`);
+
+            // first slash should pass
+            await slasher.slashDuplicatePropose(
+                height,
+                round,
+                hexBlockhash1,
+                validRound1,
+                sigString1,
+                hexBlockhash2,
+                validRound2,
+                sigString2
+            ).should.eventually.be.rejectedWith(/different signer/);
+        });
+
+        it("should slash duplicate proposals for the same height and round", async () => {
+            const darknode = darknodes[0];
+            const height = new BN("6349374925919561232");
+            const round = new BN("3652381888914236532");
+            const blockhash1 = "XTsJ2rO2yD47tg3JfmakVRXLzeou4SMtZvsMc6lkr6o";
+            const hexBlockhash1 = web3.utils.asciiToHex(blockhash1);
+            const validRound1 = new BN("6345888412984379713");
+            const proposeMsg1 = generateProposeMessage(height, round, blockhash1, validRound1);
+            const hash1 = hashjs.sha256().update(proposeMsg1).digest('hex')
+            const sig1 = ecsign(Buffer.from(hash1, "hex"), darknode.privateKey);
+            const sigString1 = Ox(`${sig1.r.toString("hex")}${sig1.s.toString("hex")}${(sig1.v).toString(16)}`);
+
+            const blockhash2 = "41RLyhshTwmPyAwjPM8AmReOB/q4LLdvYpDMKt1bEFI";
+            const hexBlockhash2 = web3.utils.asciiToHex(blockhash2);
+            const validRound2 = new BN("5327204637322492082");
+            const proposeMsg2 = generateProposeMessage(height, round, blockhash2, validRound2);
+            const hash2 = hashjs.sha256().update(proposeMsg2).digest('hex')
+            const sig2 = ecsign(Buffer.from(hash2, "hex"), darknode.privateKey);
             const sigString2 = Ox(`${sig2.r.toString("hex")}${sig2.s.toString("hex")}${(sig2.v).toString(16)}`);
 
             // first slash should pass
