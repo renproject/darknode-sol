31e9f7041ca8e683c37baec1f204845d61816fe1
Vince Au <vinceau09@gmail.com>
2019-09-27 12:22:05 +1000

    contracts, test: add tests for darknode slasher

--- contracts/DarknodeSlasher/DarknodeSlasher.sol
+++ contracts/DarknodeSlasher/DarknodeSlasher.sol
@@ -25,4 +25,39 @@ contract DarknodeSlasher is Ownable {
     function blacklist(address _guilty) external onlyOwner {
         darknodeRegistry.slash(_guilty, owner(), 0);
     }
+
+    function proposeMessage(
+        uint256 _height,
+        uint256 _round,
+        bytes memory _blockhash,
+        uint256 _validRound
+    ) public pure returns (bytes memory) {
+        return abi.encodePacked(
+            "Propose(Height=", _uint2str(_height),
+            ",Round=", _uint2str(_round),
+            ",BlockHash=", string(_blockhash),
+            ",ValidRound=", _uint2str(_validRound),
+            ")"
+        );
+    }
+
+    function _uint2str(uint _i) internal pure returns (string memory _uintAsString) { /* solium-disable-line security/no-assign-params */
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len - 1;
+        while (_i != 0) {
+            bstr[k--] = byte(uint8(48 + _i % 10));
+            _i /= 10;
+        }
+        return string(bstr);
+    }
+
 }
---
+++ test/DarknodeSlasher.ts
@@ -0,0 +1,68 @@
+import BN from "bn.js";
+
+// import { config } from "../migrations/networks";
+import {
+    DarknodeRegistryInstance, DarknodeRegistryStoreInstance, DarknodeSlasherInstance,
+    RenTokenInstance,
+} from "../types/truffle-contracts";
+// import {
+//     ID, MINIMUM_BOND, MINIMUM_EPOCH_INTERVAL_SECONDS, MINIMUM_POD_SIZE, NULL, PUBK, waitForEpoch,
+// } from "./helper/testUtils";
+
+// const RenToken = artifacts.require("RenToken");
+// const DarknodeRegistryStore = artifacts.require("DarknodeRegistryStore");
+// const DarknodeRegistry = artifacts.require("DarknodeRegistry");
+const DarknodeSlasher = artifacts.require("DarknodeSlasher");
+
+contract("DarknodeSlasher", (accounts: string[]) => {
+
+    // let ren: RenTokenInstance;
+    // let dnrs: DarknodeRegistryStoreInstance;
+    // let dnr: DarknodeRegistryInstance;
+    let slasher: DarknodeSlasherInstance;
+
+    before(async () => {
+        // ren = await RenToken.deployed();
+        // dnrs = await DarknodeRegistryStore.deployed();
+        // dnr = await DarknodeRegistry.deployed();
+        slasher = await DarknodeSlasher.deployed();
+        // await dnr.updateSlasher(slasher.address);
+        // await dnr.epoch({ from: accounts[1] }).should.be.rejectedWith(/not authorized/);
+        // await waitForEpoch(dnr);
+
+        // for (let i = 1; i < accounts.length; i++) {
+        //     await ren.transfer(accounts[i], MINIMUM_BOND);
+        // }
+    });
+
+    describe("when generating messages", async () => {
+
+        it("should correctly generate the propose message", async () => {
+            const height = new BN("6349374925919561232");
+            const round = new BN("3652381888914236532");
+            const blockhash = "XTsJ2rO2yD47tg3JfmakVRXLzeou4SMtZvsMc6lkr6o";
+            const hexBlockhash = web3.utils.asciiToHex(blockhash);
+            const validRound = new BN("6345888412984379713");
+            const proposeMsg = generateProposeMessage(height, round, blockhash, validRound);
+            const rawMsg = await slasher.proposeMessage(height, round, hexBlockhash, validRound);
+            proposeMsg.should.be.equal(web3.utils.hexToAscii(rawMsg));
+        });
+
+    });
+
+    // describe("when maliciously proposing", async () => {
+
+    //     it("should slash when 2 proposals have the same height and round", async () => {
+    //         const height=6349374925919561232;
+    //         const round=3652381888914236532;
+    //         const propose1 = generateProposeMessage(height, round, "XTsJ2rO2yD47tg3JfmakVRXLzeou4SMtZvsMc6lkr6o", 6345888412984379713);
+    //         const propose2 = generateProposeMessage(height, round, "41RLyhshTwmPyAwjPM8AmReOB/q4LLdvYpDMKt1bEFI", 5327204637322492082);
+    //     });
+
+    // });
+
+    const generateProposeMessage = (height: BN, round: BN, blockHash: string, validRound: BN): string => {
+        return `Propose(Height=${height.toString()},Round=${round.toString()},BlockHash=${blockHash},ValidRound=${validRound.toString()})`;
+    }
+
+});
