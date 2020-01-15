05795b0440baf56fbbd448b627257bbf152794df
noiach <gh@noiach.com>
2020-01-15 17:14:34 +1100

    contracts: removed commented code from Protocol

--- contracts/Protocol/Protocol.sol
+++ contracts/Protocol/Protocol.sol
@@ -4,18 +4,4 @@ import "@openzeppelin/upgrades/contracts/upgradeability/InitializableAdminUpgrad
 
 /// @notice Protocol is a directory of the current contract addresses.
 /* solium-disable-next-line no-empty-blocks */
-contract Protocol is InitializableAdminUpgradeabilityProxy {
-    // uint256 public version;
-
-    // // constructor(address o, address b, bytes memory data) public {
-    // //     version = 1;
-    // // }
-
-    // function test() public {
-    //     version = 2;
-    // }
-
-    // function v() public view returns (uint256) {
-    //     return version;
-    // }
-}
+contract Protocol is InitializableAdminUpgradeabilityProxy {}
