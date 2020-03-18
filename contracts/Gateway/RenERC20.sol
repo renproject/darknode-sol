ff541fd9adf7cf579da0a54f192241ed79ea464b
noiach <gh@noiach.com>
2020-03-18 15:53:35 +1100

    contract: add upgradabililty to RenERC20

--- contracts/Gateway/RenERC20.sol
+++ contracts/Gateway/RenERC20.sol
@@ -1,7 +1,10 @@
 pragma solidity 0.5.16;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/ownership/Ownable.sol";
+import "@openzeppelin/upgrades/contracts/upgradeability/InitializableAdminUpgradeabilityProxy.sol";
+import "@openzeppelin/upgrades/contracts/Initializable.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Detailed.sol";
 
 import "../libraries/Claimable.sol";
 import "../libraries/CanReclaimTokens.sol";
@@ -9,12 +12,24 @@ import "../libraries/CanReclaimTokens.sol";
 /// @notice RenERC20 represents a digital asset that has been bridged on to
 /// the Ethereum ledger. It exposes mint and burn functions that can only be
 /// called by it's associated Gateway contract.
-contract RenERC20 is ERC20, ERC20Detailed, Claimable, CanReclaimTokens {
+contract RenERC20 is
+    Initializable,
+    ERC20,
+    ERC20Detailed,
+    Claimable,
+    CanReclaimTokens
+{
     /* solium-disable-next-line no-empty-blocks */
-    constructor(string memory _name, string memory _symbol, uint8 _decimals)
-        public
-        ERC20Detailed(_name, _symbol, _decimals)
-    {}
+    function initialize(
+        address _nextOwner,
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals
+    ) public initializer {
+        ERC20Detailed.initialize(_name, _symbol, _decimals);
+        Claimable.initialize(_nextOwner);
+        CanReclaimTokens.initialize(_nextOwner);
+    }
 
     function burn(address _from, uint256 _amount) public onlyOwner {
         _burn(_from, _amount);
@@ -29,10 +44,10 @@ contract RenERC20 is ERC20, ERC20Detailed, Claimable, CanReclaimTokens {
 /// but are used to track deployments.
 
 /* solium-disable-next-line no-empty-blocks */
-contract renBTC is RenERC20("renBTC", "renBTC", 8) {}
+contract renBTC is InitializableAdminUpgradeabilityProxy {}
 
 /* solium-disable-next-line no-empty-blocks */
-contract renZEC is RenERC20("renZEC", "renZEC", 8) {}
+contract renZEC is InitializableAdminUpgradeabilityProxy {}
 
 /* solium-disable-next-line no-empty-blocks */
-contract renBCH is RenERC20("renBCH", "renBCH", 8) {}
+contract renBCH is InitializableAdminUpgradeabilityProxy {}
